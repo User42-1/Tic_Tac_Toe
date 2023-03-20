b1f727e6aaa5f9e7a94bb25050c3f66fdca2b870
User42-1 <jens.schrecker@t-online.de>
2023-03-20 01:16:31 +0100

    Occupied fields disabled + sound

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 let fields = []
 let current_player = 'cross';
 
+let AUDIO_WRONG = new Audio('./audio/wrong.wav');
+let AUDIO_WIN = new Audio('./audio/win.wav');
+
+function select_field(field_number) {
+    if (!fields[field_number]) {   /* if field not occupied */
+    change_current_player();
+    fields[field_number] = current_player;
+    console.log(fields);
+    render_fields();
+    check_for_winner();
+    } else {
+        AUDIO_WRONG.play();
+        }
+}
+
 function change_current_player() {
     if (current_player == 'cross') {
         current_player = 'circle';
+        document.getElementById('player_1').classList.add('player_inaktive');
+        document.getElementById('player_2').classList.remove('player_inaktive');
     } else {
         current_player = 'cross';
+        document.getElementById('player_2').classList.add('player_inaktive');
+        document.getElementById('player_1').classList.remove('player_inaktive');
     }
-    return current_player;
-}
-
-function select_field(field_number) {
-    fields[field_number] = change_current_player();
-    console.log(fields);
-    render_fields();
 }
 
 function render_fields() {
@@ -24,5 +36,37 @@ function render_fields() {
         else if (fields[i] == 'circle') {
             document.getElementById('circle_'+i).classList.remove('d_none');
         }  
+    }
+}
+
+function check_for_winner() {
+    let winner;
+    if (fields[0] == fields[1] && fields[0] == fields[2]) {
+        winner = fields[0];
+    }
+    else if (fields[3] == fields[4] && fields[3] == fields[5]) {
+        winner = fields[3];
+    }
+    else if (fields[6] == fields[7] && fields[6] == fields[8]) {
+        winner = fields[6];
+    }
+    else if (fields[0] == fields[3] && fields[0] == fields[6]) {
+        winner = fields[0];
+    }
+    else if (fields[1] == fields[4] && fields[1] == fields[7]) {
+        winner = fields[1];
+    }
+    else if (fields[2] == fields[5] && fields[2] == fields[8]) {
+        winner = fields[2];
+    }
+    else if (fields[0] == fields[4] && fields[0] == fields[8]) {
+        winner = fields[0];
+    }
+    else if (fields[2] == fields[4] && fields[2] == fields[6]) {
+        winner = fields[2];
+    }
+    if (winner) {
+        console.log(winner + ' hat gewonnen');
+        AUDIO_WIN.play();
     }
 }
